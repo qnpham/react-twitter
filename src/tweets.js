53541e3b098e2563195e738560108961c355409e
Quan Pham <mockanterr@gmail.com>
2023-01-16 22:15:34 +0700

    made basic tweet better

--- src/tweets.js
+++ src/tweets.js
@@ -1,20 +1,25 @@
 function Basic() {
   return (
     <div className="basic">
-      <div className="basic-top">
-        <div className="tweet-pfp"></div>
-        <div className="user">
-          <span className="alias">alias</span>
-          <span className="username">@username</span>
-          <span className="time">* 20h</span>
+      <div className="column-full card">
+        <div className="pfp-container col-20">
+          <div className="tweet-pfp"></div>
+        </div>
+        <div className="col-80">
+          <div className="user">
+            <span className="alias">alias</span>
+            <span className="username">@username</span>
+            <span className="time">* 20h</span>
+            <p className="basic-text">
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vitae
+              quas laborum nulla quo iusto recusandae nihil a fugiat eveniet
+              accusamus consequuntur praesentium debitis doloribus, nam, odit
+              fuga libero repellat provident.
+            </p>
+          </div>
         </div>
       </div>
-      <p className="basic-text">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro,
-        temporibus quas. Eos, animi consectetur voluptates deleniti eveniet
-        ullam eaque! Officia eligendi
-      </p>
-      <div className="icons">
+      <div className="icons column-full">
         <i class="fa-regular fa-message"></i>
         <i class="fa-solid fa-retweet"></i>
         <i class="fa-regular fa-heart"></i>
